a29e25707d8c7ae379dfb3076ad2e10053cfde32
LaSpadaIlaria <ilalaspada@gmail.com>
2025-10-18 10:53:57 +0200

    prima parte: definizione delle righe e delle colonne
    ho iniziato a caricare il dataset, ho stabilito le righe e le colonne e sto iniziando a costruire il ciclo for

--- sketch.js
+++ sketch.js
@@ -1,66 +1,33 @@
-//2 creo una variabile globale chiamata table//
 let table;
 
-
-//1 carico i dati nel preload//
 function preload() {
-  table = loadTable("assets/dataset.csv", "csv", "header");
-  //3 per vedere se ho caricato i dati devo://
-  console.log(table);
-  
-  //4 come riesco a mettere i miei dati in una griglia con un pudding nel margine e una sistemazione con colonne righe.  per fare in modo che la pagina sia grande come il mio schermo devo scrivere windowWith, wondowHeight. Devo decidere quanti sono gli elementi, quando è l'ingombro massimo degli elementi, quanto è il pudding dei vari elementi, quanti elementi ci sono in una riga quindi quante righe mi servono per metterli tutti//
+  // Carica il dataset CSV
+  table = loadTable("assets/dataset_2.csv", "csv", "header");
 }
 
-//5 creiamo una variabile per definire il pudding esterno//
 function setup() {
-  
-  //console.log(table);//
+  // Controllo se ho caricato i dati
+  let outerPadding = 55; //spazio vuoto tra i bordi del canvas e la griglia di elementi.
+  let padding = 15;  //spazio tra gli elementi (tra una “cella” e l’altra della griglia).
+  let itemSize = 37; //dimensione (lato) di ogni “oggetto” o cella da disegnare.
 
-  let outerPadding = 20; 
-  let padding = 18;
-  let itenSize= 30; //questa è per la grandezza degli elementi
-  
-  
+  // Calcolo il numero di colonne
+  let cols = floor((windowWidth - outerPadding * 2) / (itemSize + padding)); //(windowWidth - outerPadding * 2) → prende la larghezza totale della finestra e toglie i margini esterni sinistro e destro. (itemSize + padding) → è lo spazio totale occupato da ogni elemento (dimensione + spazio tra elementi).floor() → arrotonda per difetto (es. 7.8 diventa 7), così non si disegna una colonna “incompleta”
+  let rows = ceil(table.getRowCount() / cols); //restituisce quante righe ci sono nel CSV (cioè quanti “dati” totali). Dividendo per cols si calcola quante righe servono per mostrare tutti i dati. ceil() → arrotonda per eccesso (es. 4.2 → 5), perché serve una riga in più anche se l’ultima non è piena.
+  let totalHeight = outerPadding * 2 + rows * itemSize + (rows - 1) * padding; //Calcola l’altezza totale del canvas. 
 
-  //6 devo calcolare il numero delle colonne nella larghezza della pagina. queesto numero che esce potrebbe non essere intero, quindi gli dico di arrotondarlo per difetto mettendo il floor//
-  let cols = floor ((windowWidth - outerPadding *2) / (itenSize + padding));
+  // Creo il canvas
+  createCanvas(windowWidth, totalHeight); //crea lo spazio di disegno con la larghezza pari alla finestra
+  background(252, 249, 234);
 
-  //7 voglio vedere se funziona fino ad ora//
-  //console.log("colonne: ", cols);//
+  console.log("cols: ", cols, " rows: ", rows); //verifico che il numero totale di righe e colonne siano corretti
 
-  //8 voglio una seconda variabile per sapere quanti righe ci sono////9 devo arrotondate quindi per eccesso//
-  let rows = ceil(table.getRowCount() / cols);
+  let colCount = 0; 
+  let rowCount = 0; //servono per tenere traccia della posizione nella griglia. ogni volta che disegno un glifo rolcount aumenta, poi quando arrivo alla fine della riga si resetta tornando a zero, ma aumenta di 1 rowcount. 
 
-  
+  // Itero attraverso tutte le righe del dataset
+  for (let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber++) { //for serve a ripetere un’operazione per ogni riga. table.getRowCount() restituisce quante righe ci sono nel CSV.
+    // Carico dati della riga
+    let data = table.getRow(rowNumber).obj; //Prende la riga con indice rowNumber es. rownumber = 0 restituisce un oggetto speciale di tipo p5.TableRow, cioè una riga della tabella che contiene i valori. in questo caso obj È una proprietà di p5.TableRow che converte la riga in un normale oggetto JavaScript.
 
-  let totalHeight = outerPadding * 2 + rows * itenSize + (rows - 1) * padding; //questo mi serve per capire l'altezza totale
-
-  
-  
-  //creo il canvas//
-  createCanvas (windowWidth, windowHeight);
-  background ("coral");
-
-console.log("cols: ", cols, "rows: ", rows);
-
-  let colCount = 0;
-
-  for(let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber) {
     
-    //carico dati della riga//
-    let data = table.getRow(rowNumber).obj;
-    
-    let xPos = outerPadding + colCount * (itenSize + padding);
-
-    //vediamo se il ciclo funziona disegnando un rettangolo//
-    rect(xPos, 50, itenSize, itenSize);
-    //aumento con colcount//
-    colCount++;
-
-
-  }
-}
-
-function draw() {
-  // put drawing code here
-}
